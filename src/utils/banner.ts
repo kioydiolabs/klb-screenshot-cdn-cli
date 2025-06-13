d9fbafdf9e2217c59c7eea311db32f6c3eeb9459
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-06-14 02:06:07 +0300

    remove banner

--- src/utils/banner.ts
+++ src/utils/banner.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /*
  * Copyright 2025 KioydioLabs
  *
@@ -10,36 +8,12 @@
  * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { program } from "commander";
-import dotenv from "dotenv";
-import updateNotifier from "update-notifier";
-import packageJson from "../package.json" with { type: "json" };
-
-dotenv.config();
-
-const notifier = updateNotifier({ pkg: packageJson, updateCheckInterval: 0 });
-notifier.notify({ isGlobal: true, defer: false });
-
-program.version(
-  packageJson.version,
-  "-v, --version",
-  "Output the current version",
-);
-
-import { configureCredentialsCommand } from "./commands/configure-credentials";
-import { deleteCommand } from "./commands/delete";
-import { purgeCacheCommand } from "./commands/purge-cache";
-import { banner } from "./utils/banner";
-
-program.addCommand(configureCredentialsCommand);
-program.addCommand(deleteCommand);
-program.addCommand(purgeCacheCommand);
-
-// If no command is provided, display help
-if (!process.argv.slice(2).length) {
-  banner();
-  program.outputHelp();
-  process.exit(0);
+export function banner() {
+  console.log(
+    "\n----------------------------------------------------------------",
+  );
+  console.log("KioydioLabs Screenshot CDN CLI (C) 2025");
+  console.log(
+    "----------------------------------------------------------------\n",
+  );
 }
-
-program.parse(process.argv);
