d8abeb2713d87e1ed75146a5667ad5ae9b6fbee2
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-06-14 04:19:45 +0300

    batch file deletion working

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -21,10 +21,11 @@ import {
 
 import Table from "cli-table3";
 import chalk from "chalk";
+import ora from "ora";
 
 import { loadCredentials } from "../utils/credentials.js";
 import { purgeCloudflareCache } from "../utils/cloudflare.js";
-import { fileObject } from "../utils/types";
+import { fileObject, fileObjectDeleted } from "../utils/types";
 
 export const deleteCommand = new Command()
   .command("delete [urls...]")
@@ -62,6 +63,7 @@ export const deleteCommand = new Command()
 
       // array of all items that will be fetched
       let filesFetched: fileObject[] = [];
+      let skippedUrls: string[] = [];
 
       // table object for cli-table3
       const table = new Table({
@@ -92,6 +94,7 @@ export const deleteCommand = new Command()
               ? prettyBytes(result.ContentLength)
               : "-",
             date: result.LastModified,
+            key: key,
           };
           filesFetched.push(fileObject);
           const tableFileObject = [
@@ -102,14 +105,148 @@ export const deleteCommand = new Command()
           table.push(tableFileObject);
         } catch (e) {
           if (e instanceof Error) {
-            console.error(e.message, `Skipping file "${url}"`);
+            skippedUrls.push(url);
           }
         }
       });
 
       await Promise.all(promises);
 
+      if (filesFetched.length < 1) {
+        console.log(chalk.ansi256(202)("No files found."));
+        process.exit(1);
+      }
+
+      console.log(
+        chalk.bgWhiteBright.black(
+          "Job overview/preview: The following actions will be performed:\n",
+        ),
+      );
+
+      console.log(
+        chalk.greenBright("These files were found. They will be") +
+          chalk.redBright(" DELETED PERMANENTLY:"),
+      );
       console.log(table.toString());
+
+      if (skippedUrls.length >= 1) {
+        console.log(
+          chalk.ansi256(202)(
+            "Some URLs however don't seem to point to existing files on the bucket. These will be skipped:",
+          ),
+        );
+        skippedUrls.forEach((url: string) => {
+          console.log(`${url}`);
+        });
+        console.log("\n");
+      }
+
+      if (!options.force) {
+        const answer = await inquirer.prompt([
+          {
+            type: "confirm",
+            name: "confirmDelete",
+            message: "Do you actually want to proceed?",
+            default: false,
+          },
+        ]);
+
+        if (!answer.confirmDelete) {
+          console.log("Cancelled.");
+          process.exit(0);
+        }
+      }
+
+      if (!options.force && urls.length > 1) {
+        const answer = await inquirer.prompt([
+          {
+            type: "confirm",
+            name: "confirmDelete2",
+            message:
+              "Looks like this is a batch job. Confirm one last time that you really want these gone.",
+            default: false,
+          },
+        ]);
+
+        if (!answer.confirmDelete2) {
+          console.log("Cancelled.");
+          process.exit(0);
+        }
+      }
+
+      const spinner = ora(chalk.green("Attempting to delete files...")).start();
+
+      let filesDeleted: fileObjectDeleted[] = [];
+      const deletedTable = new Table({
+        head: ["URL", "Size", "Date uploaded", "Deleted successfully"],
+        // colWidths: [60, 15],
+        style: { head: ["cyan"] },
+      });
+      const deletePromises = filesFetched.map(async (obj: fileObject) => {
+        try {
+          await s3.send(
+            new DeleteObjectCommand({
+              Bucket: bucketName,
+              Key: obj.url,
+            }),
+          );
+          filesDeleted.push({
+            url: obj.url,
+            date: obj.date,
+            key: obj.key,
+            size: obj.size,
+            deleted: true,
+          });
+          const tableDeletedObject = [
+            obj.url,
+            obj.size,
+            obj.date?.toString() ?? "",
+            "✔",
+          ];
+          deletedTable.push(tableDeletedObject);
+        } catch (e) {
+          if (e instanceof Error) {
+            filesDeleted.push({
+              url: obj.url,
+              date: obj.date,
+              key: obj.key,
+              size: obj.size,
+              deleted: false,
+            });
+            const tableDeletedObject = [
+              obj.url,
+              obj.size,
+              obj.date?.toString() ?? "",
+              "✗",
+            ];
+            deletedTable.push(tableDeletedObject);
+          }
+        }
+      });
+
+      await Promise.all(deletePromises);
+      spinner.succeed("Job done.");
+
+      console.log(
+        chalk.bgWhiteBright.black.bold(`\n\nJob overview:\n`) +
+          `URLs Provided by user: ${urls.length}\nFiles found: ${filesFetched.length}\nFiles deleted successfully: ${filesDeleted.length}/${filesFetched.length}\n\n `,
+      );
+
+      if (filesDeleted.length > 1) {
+        const answer = await inquirer.prompt([
+          {
+            type: "confirm",
+            name: "showJobResults",
+            message: "Show job results in detail?",
+            default: false,
+          },
+        ]);
+
+        if (answer.showJobResults) {
+          console.log(deletedTable.toString());
+        }
+      }
+
       process.exit(0);
     },
   );
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,4 +8,17 @@
  * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-export type fileObject = { url: string; size: string; date?: Date };
+export type fileObject = {
+  url: string;
+  size: string;
+  date?: Date;
+  key: string;
+};
+
+export type fileObjectDeleted = {
+  url: string;
+  size: string;
+  date?: Date;
+  key: string;
+  deleted: boolean;
+};
