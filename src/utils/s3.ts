969fedd0ed1928e776643011cb9c9254efb82cfb
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-06-17 06:22:38 +0300

    add s3 misc commands in utils

---
+++ src/utils/s3.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2025 KioydioLabs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import fs from "fs";
+
+export async function uploadFile(
+  s3: S3Client,
+  uploadParams: {
+    Bucket: string;
+    Key: string;
+    Body: fs.ReadStream;
+  },
+) {
+  try {
+    const data = await s3.send(new PutObjectCommand(uploadParams));
+    console.log("Upload success", data);
+  } catch (err) {
+    console.error("Upload error", err);
+  }
+}
