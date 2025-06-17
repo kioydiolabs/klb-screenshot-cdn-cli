81927fbfb3ef2622f57c4bb57cd9c352608f8cd6
Stratos Thivaios <sthivaios@kioydiolabs.org>
2025-06-17 07:06:29 +0300

    add constructFileURL function

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -8,6 +8,8 @@
  * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+import { loadCredentials } from "./credentials.js";
+
 export function capitalizeFirstLetter(inputString: string) {
   return (
     String(inputString).charAt(0).toUpperCase() + String(inputString).slice(1)
@@ -22,4 +24,9 @@ export function generateRandomID(length: number) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
+}
+
+export function constructFileUrl(key: string) {
+  const { domain } = loadCredentials();
+  return `https://${domain}/${key}`;
 }
